44d6c061fdf694ffca2fd978f427648db413eba5
zhangyanrong <479496771@qq.com>
2018-09-30 17:46:44 +0800

    ''

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser'); //能接收post数据
+const crypto = require('crypto');
 const server = express();
 
 
@@ -8,13 +9,27 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json()); // for parsing application/json
 server.use(multer());
 
-//服务器的webhook配置
-const exec = require('child_process').execSync;
-server.post('/webhook', (req, res) => {
-    console.log(req.body,'//////////////////////////////////////////')
-    console.log(req.body['token'],1)
-    console.log(req.headers['x-coding-event'],2)
-    if ('hooks' === req.body['token']) {
+
+const verifyWebhook = (req) => {
+    if (!req.headers['user-agent'].includes('Coding.net Hook')) {
+        return false;
+    }
+    // Compare their hmac signature to our hmac signature
+    // (hmac = hash-based message authentication code)
+    const theirSignature = req.headers['x-coding-signature'];
+    console.log(theirSignature,1111111111111);
+    const payload = JSON.stringify(req.body);
+    console.log(payload,2222222222222222)
+    const secret = process.env.SECRET_TOKEN;
+    console.log(secret,3333333333333333)
+    const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+    console.log(ourSignature,44444444444444444444)
+    console.log(crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature)),55555555555555555555)
+    return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+};
+
+const hooksPull = () =>{
+    if ('hooks' === req.headers['X-Coding-Signature']) {
         console.log(3)
         exec('git pull', {'cwd': '/var/www/webserver'},
             (error, stdout, stderr) => {
@@ -43,7 +58,11 @@ server.post('/webhook', (req, res) => {
                     console.log('构建成功')
                 }
             })
-    } else if ('myadmin' === req.body['token']) {
+    }
+}
+
+const myAdminPull = () =>{
+    if ('myadmin' === req.headers['X-Coding-Signature']) {
         console.log(4)
         exec('git pull', {'cwd': '/var/www/myadmin'},
             (error, stdout, stderr) => {
@@ -72,10 +91,20 @@ server.post('/webhook', (req, res) => {
                     console.log('构建成功')
                 }
             })
-    } else {
-        console.log(' failed token ===================================');
-        res.send('<pre>token不正确?</pre>');
     }
+}
+
+
+//服务器的webhook配置
+const exec = require('child_process').execSync;
+server.post('/webhook', (req, res) => {
+    if(verifyWebhook(req)){
+
+
+    }else {
+        console.log('失败')
+    }
+
 });
 
 server.listen(6666);
