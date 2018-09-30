7c669ab30b2c57b6e46d88c9cbcd998076679c34
zhangyanrong <479496771@qq.com>
2018-09-30 15:38:44 +0800

    ''

--- webhook.js
+++ webhook.js
@@ -41,6 +41,34 @@ server.post('/webhook', (req, res) => {
                     console.log('构建成功')
                 }
             })
+    } else if ('myadmin' === req.body['token']) {
+        exec('git pull', {'cwd': '/var/www/myadmin'},
+            (error, stdout, stderr) => {
+                console.log('stdout========================\n' + stdout + '====================================');
+                console.log('stderr========================\n' + stderr + '====================================');
+                if (error !== null) {
+                    res.send('<pre>fail!!!\n' + stdout + error + '</pre>');
+                } else {
+                    res.send('<pre>done!!!\n' + stdout + '</pre>');
+                    console.log('push成功！')
+                }
+            });
+        exec('npm install',{'cwd':'/var/www/myadmin'},
+            (error,stdout,stdin) =>{
+                if (error !== null){
+                    console.log('失败')
+                }else{
+                    console.log('构建成功')
+                }
+            })
+        exec('npm run build',{'cwd':'/var/www/myadmin'},
+            (error,stdout,stdin) =>{
+                if (error !== null){
+                    console.log('失败')
+                }else{
+                    console.log('构建成功')
+                }
+            })
     } else {
         console.log(' failed token ===================================');
         res.send('<pre>token不正确?</pre>');
