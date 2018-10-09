e481f4c4b180170dba3f9394b4f3b68f47e2060d
zhangyanrong <479496771@qq.com>
2018-10-09 11:05:18 +0800

    'fgfg'

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const verifyWebhook = (req) => {
     console.log(theirSignature);
     const payload = JSON.stringify(req.body);
     // const secret = process.env.SECRET_TOKEN;
-    const secret = 123;
+    const secret = '123';
     const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
     console.log(crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature)))
     return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
