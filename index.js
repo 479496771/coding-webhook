1e51f2cbd70b7bef4780337231216bb2590fa406
zhangyanrong <479496771@qq.com>
2018-10-09 16:28:18 +0800

    'dfdf'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const verifyWebhook = (req) => {
     const theirSignature = req.headers['x-coding-signature'];
     console.log(theirSignature);
     const payload = req.body;
-    const secret = process.env.MYADMIN_TOKEN;
+    const secret = 'myadmin';
     console.log(secret)
     const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
     console.log(ourSignature)
